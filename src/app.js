3f89ffd08063f3210bfd01218b435f7048cf47ac
McIlroy, Stephanie A <stephanie.a.mcilroy@accenture.com>
2019-02-15 12:42:03 -0500

    fixed urlencoded method

--- src/app.js
+++ src/app.js
@@ -2,16 +2,15 @@ const fs = require('fs');
 const path = require('path')
 const express = require('express')
 
-
 const app = express()
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.urlencoded, {
+app.use(express.urlencoded({
     extended: true
-})
+}));
 
 const accountData = fs.readFileSync(path.join(__dirname, 'json', 'accounts.json'), 'utf8');
 const accounts = JSON.parse(accountData);
